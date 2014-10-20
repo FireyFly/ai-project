9ba62ec97f6d5a18fbf603ad29854ef4e45f5023
John Eriksson <johne2@kth.se>
2014-10-20 15:35:22 +0200

    Minor rewrite of FrequencyMap
    Added a Map view and used getOrDefault in methods

--- src/FrequencyList.java
+++ src/FrequencyList.java
@@ -2,26 +2,86 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.*;
 
-/** Represents a frequency distribution of T's by mapping T's to their number
- *  of occurrences. */
+/**
+ * Represents a frequency distribution of T's by mapping T's to their number of
+ * occurrences.
+ */
 public class FrequencyList<T> implements Serializable {
+
     private int totalCount = 0;
     private Map<T, Integer> counts = new HashMap<>();
 
     public void add(T t) {
         this.totalCount++;
-        Integer count = this.counts.get(t);
-        this.counts.put(t, count == null? 1 : count + 1);
+        Integer count = this.counts.getOrDefault(t, 0);
+        this.counts.put(t, count + 1);
     }
 
     public double get(T t) {
-        Integer count = this.counts.get(t);
-        return count == null? 0 : (double) count / this.totalCount;
+        if (totalCount == 0) {
+            return Double.NaN;
+        }
+        Integer count = this.counts.getOrDefault(t, 0);
+        return (double) count / this.totalCount;
+    }
+
+    public Map<T, Double> getProbabilityMap() {
+        return new AbstractMap<T, Double>() {
+
+            @Override
+            public Double get(Object key) {
+                if (totalCount == 0) {
+                    return Double.NaN;
+                }
+                double d = counts.getOrDefault(key, 0);
+                return d / totalCount;
+            }
+
+            @Override
+            public Set<Map.Entry<T, Double>> entrySet() {
+                return new AbstractSet<Map.Entry<T, Double>>() {
+
+                    @Override
+                    public Iterator<Map.Entry<T, Double>> iterator() {
+
+                        return new Iterator<Entry<T, Double>>() {
+                            Iterator<Map.Entry<T, Integer>> iter1 = counts.entrySet().iterator();
+
+                            @Override
+                            public boolean hasNext() {
+                                return iter1.hasNext();
+                            }
+
+                            @Override
+                            public Map.Entry<T, Double> next() {
+                                Entry<T, Integer> entry1 = iter1.next();
+                                int i = entry1.getValue();
+                                double r;
+                                if(totalCount == 0){
+                                    r = Double.NaN;
+                                } else {
+                                    r = ((double) i) / totalCount;
+                                }
+                                return new SimpleImmutableEntry<>(entry1.getKey(), r);
+                            }
+                        };
+
+                    }
+
+                    @Override
+                    public int size() {
+                        return counts.size();
+                    }
+
+                };
+            }
+        };
+
     }
 
     public Stream<Utils.Pair<T, Double>> stream() {
-      return counts.entrySet().stream()
-                   .map(entry -> new Utils.Pair<>(entry.getKey(),
-                                                  (double) entry.getValue() / totalCount));
+        return counts.entrySet().stream()
+                .map(entry -> new Utils.Pair<>(entry.getKey(),
+                                (double) entry.getValue() / totalCount));
     }
 }
