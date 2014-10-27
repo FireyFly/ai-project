e60d0f923e0226dba9c39dc7d08abd48975d5e00
John Eriksson <johne2@kth.se>
2014-10-27 18:39:20 +0100

    Tested sentence generator

--- src/SentenceGenerator.java
+++ src/SentenceGenerator.java
@@ -4,23 +4,63 @@ import java.util.*;
 import java.util.stream.Stream;
 
 public class SentenceGenerator {
-    
+
     private static class Interval {
-       final double start, end;
+
+        final double start, end;
 
         public Interval(double start, double end) {
-            if(end < start || Double.isNaN(start) || Double.isNaN(end)){
+            if (end < start || Double.isNaN(start) || Double.isNaN(end)) {
                 throw new IllegalArgumentException();
             }
             this.start = start;
             this.end = end;
         }
-        
-        public boolean isInInterval(double d){
+
+        public boolean isInInterval(double d) {
             return d >= start && d <= end;
         }
-       
-        
+
+    }
+
+    public static void generateSentence(Model model, String start) {
+        System.out.print(start + "");
+        StringBuilder line = new StringBuilder(start);
+        int dotcount = 0;
+        while (true) {
+            List<Utils.Pair<Markov.Symbol<String>, Double>> list = Utils.toList(model.predictNext(line.toString()));
+            double sum = 0;
+            for (Utils.Pair<Markov.Symbol<String>, Double> p : list) {
+                sum += p.e2;
+            }
+            double r = Math.random();
+            Markov.Symbol<String> found = null;
+            for (Utils.Pair<Markov.Symbol<String>, Double> p : list) {
+                double prop = p.e2 / sum;
+                if (r <= prop) {
+                    found = p.e1;
+                    break;
+                }
+                r -= prop;
+            }
+            if (found == null) {
+                break;
+            } else if (found.isSpecial()) {
+                break;
+            } else {
+                if (line.length() != 0) {
+                    line.append(" ");
+                    System.out.print(" ");
+                }
+                line.append(found.getValue());
+                System.out.print(found.getValue());
+                
+                if(found.getValue().equals(".") && ++dotcount == 3){
+                    break;
+                }
+            }
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) throws ClassNotFoundException, RuntimeException {
@@ -45,38 +85,9 @@ public class SentenceGenerator {
                 System.err.println("Couldn't construct model '" + args[0] + "'.");
                 System.exit(1);
             }
-            String start = "";
-            StringBuilder line = new StringBuilder(start);
-            while (true) {
-                Stream<Utils.Pair<Markov.Symbol<String>, Double>> next = model.predictNext(line.toString());
-                double sum = next.mapToDouble(x -> x.e2).sum();
-                List<Utils.Pair<Markov.Symbol<String>, Double>> list = Utils.toList(next);
-                double r = Math.random();
-                Markov.Symbol<String> found = null;
-                for(Utils.Pair<Markov.Symbol<String>, Double> p: list){
-                    double prop = p.e2/sum;
-                    if(r < prop){
-                        found = p.e1;
-                        break;
-                    }
-                    r -= prop;
-                }
-                if(found == null){
-                    throw new IllegalStateException();
-                } else if(found.isSpecial()){
-                    break;
-                } else {
-                    if(line.length() != 0){
-                        line.append(" ");
-                        System.out.println(" ");
-                    }
-                    line.append(found.getValue());
-                    System.out.println(found.getValue());
-                }                  
+            for(int i=0;i<100;i++){
+                generateSentence(model, "how");
             }
-
-            
-            
 
         } catch (IOException ex) {
             throw new RuntimeException(ex);
